459e7aad428915d77197a3276e405d47ad106694
Artur Karpovich <onelifeitsme@gmail.con>
2021-11-03 21:30:56 +0300

    item photo confines made

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Product)
+admin.site.register(Category)
 
 class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
--- shop/product/models.py
+++ shop/product/models.py
@@ -3,6 +3,18 @@ from django.urls import reverse
 
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя товара')
@@ -11,7 +23,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
     sizes = models.IntegerField(default=1, verbose_name='Размеры')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена товара')
-    # category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория товара')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория товара')
     promotional = models.BooleanField(default=False, verbose_name='На акции')
 
 
