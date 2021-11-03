c7c1f21d853878321951fcf58803944861217131
Artur Karpovich <onelifeitsme@gmail.con>
2021-11-03 22:06:33 +0300

    item photo confines made

--- shop/shop_main_app/views.py
+++ shop/shop_main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
-from product.models import *
+from product.models import Product, Category
 
 # Create your views here.
 def index(requst):
-    # cats = Category.objects.all()
+    categories = Category.objects.all()
     products = Product.objects.all()
     promotionals = Product.objects.filter(promotional=True)
-    return render(requst, 'shop_main_app/index.html', {'products': products, 'promotionals': promotionals})
+    return render(requst, 'shop_main_app/index.html', {'categories': categories, 'products': products, 'promotionals': promotionals})
 
 
 
